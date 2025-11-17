1bee75146b282f49009621c07acf79f83338e7e0
hyojun01 <joon_1234@kookmin.ac.kr>
2025-11-17 20:38:03 +0900

    MODIFY Makefile

---
+++ hls/Makefile
@@ -0,0 +1,10 @@
+TOPTARGETS := all clean
+
+SUBDIRS := $(filter-out $(wildcard _*/.), $(wildcard */.))
+
+$(TOPTARGETS): $(SUBDIRS)
+
+$(SUBDIRS):
+	$(MAKE) -C $@ $(MAKECMDGOALS)
+
+.PHONY: $(TOPTARGETS) $(SUBDIRS)
--- hls/low_pass_filter_second/low_pass_filter_second.tcl
+++ hls/low_pass_filter_second/low_pass_filter_second.tcl
@@ -1,6 +1,6 @@
 open_project -reset low_pass_filter_second
 set_top low_pass_filter_second
-add_file low_pass_filter_seond.cpp
+add_file low_pass_filter_second.cpp
 open_solution solution1 -reset
 set_part {xc7z010clg225-1}
 create_clock -period 10ns
