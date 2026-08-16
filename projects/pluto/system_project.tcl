9e855e3f520857f0493c49fbee83383ba031cbf3
hyojun01 <joon_1234@kookmin.ac.kr>
2026-08-16 12:29:36 +0900

    projects/pluto/system_project.tcl: modify the order of AXI CDC XDC and route XDC

--- projects/pluto/system_project.tcl
+++ projects/pluto/system_project.tcl
@@ -15,6 +15,14 @@ adi_project_files pluto [list \
   "$ad_hdl_dir/library/common/ad_iobuf.v"]
 
 set_property is_enabled false [get_files  *system_sys_ps7_0.xdc]
+set dut_cdc_xdc \
+  [get_files -all *HDL_DUT_ip_src_HDL_DUT_interface_constraints.xdc]
+
+if {[llength $dut_cdc_xdc] != 1} {
+  error "Expected one HDL DUT CDC XDC, found [llength $dut_cdc_xdc]"
+}
+
+set_property PROCESSING_ORDER LATE $dut_cdc_xdc
 adi_project_run pluto
 source $ad_hdl_dir/library/axi_ad9361/axi_ad9361_delay.tcl
 
